33bf37dce3732687eaad708e0dde1ac09b076d01
cdsupina <cdsupina@gmail.com>
2019-06-13 12:00:00 -0400

    can_spawn new returns new x position as an option

--- src/components/spawner.rs
+++ src/components/spawner.rs
@@ -1,9 +1,15 @@
-use amethyst::ecs::prelude::{Component, DenseVecStorage};
+use amethyst::{
+    ecs::prelude::{Component, DenseVecStorage},
+};
 
 use std::{
     collections::{HashMap},
     vec::Vec,
 };
+
+
+use crate::space_shooter::{ARENA_MAX_X, ARENA_MIN_X, ARENA_SPAWN_OFFSET};
+use rand::{thread_rng, Rng};
 
 
 #[derive(Clone)]
@@ -20,24 +26,27 @@ pub struct Spawner<T> {
     pub spawn_counter: u32,
 }
 
-impl<'a, T> Component for Spawner<T> where T: Send + Sync + 'static{
+impl<'a, T> Component for Spawner<T> where T: Send + Sync + 'static {
     type Storage = DenseVecStorage<Self>;
 }
 
 impl<'a, T> Spawner<T> where T: Send + Sync + 'static {
 
     //updates or resets the spawn timer, returns true if can spawn, false if can't
-    pub fn can_spawn(&mut self, dt: f32) -> bool {
+    pub fn can_spawn(&mut self, dt: f32) -> Option<f32> {
         if self.pool.spawn_list.len() == 0 {
-            return false;
+            return None;
         }
         if self.spawn_timer > 0.0 {
-            self.spawn_timer -=  dt;
-            return false;
+            self.spawn_timer -= dt;
+            return None;
         } else {
             self.spawn_timer = self.spawn_interval;
-            return true;
+
+            let max_width = ARENA_MAX_X - ARENA_SPAWN_OFFSET;
+            let min_width = ARENA_MIN_X + ARENA_SPAWN_OFFSET;
+            let new_x = ARENA_MIN_X + ARENA_SPAWN_OFFSET + thread_rng().gen::<f32>()* (max_width - min_width);
+            return Some(new_x);
         }
     }
-
 }
