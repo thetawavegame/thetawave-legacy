13ebd6fe69043513540c3525b1e1f3505a3a45a1
Carlo Supina <cdsupina@gmail.com>
2019-09-01 18:09:10 -0400

    add sound effects for item pickup, laser, crash, explosion and hit

--- src/systems/player_hit.rs
+++ src/systems/player_hit.rs
@@ -1,11 +1,16 @@
 use amethyst::{
     core::transform::Transform,
-    ecs::prelude::{Entities,Join, ReadStorage, System, WriteStorage},
+    ecs::prelude::{Entities,Join, ReadStorage, System, WriteStorage, Read, ReadExpect},
+    audio::{output::Output, Source},
+    assets::AssetStorage,
 };
+
+use std::ops::Deref;
 
 use crate::{
     components::{Enemy, Blast},
     systems::hitbox_collide,
+    audio::{play_sfx, Sounds},
 };
 
 
@@ -17,9 +22,12 @@ impl<'s> System<'s> for PlayerHitSystem {
         WriteStorage<'s, Enemy>,
         WriteStorage<'s, Blast>,
         ReadStorage<'s, Transform>,
+        Read<'s, AssetStorage<Source>>,
+        ReadExpect<'s, Sounds>,
+        Option<Read<'s, Output>>
     );
 
-    fn run(&mut self, (entities, mut enemies, mut blasts, transforms): Self::SystemData) {
+    fn run(&mut self, (entities, mut enemies, mut blasts, transforms, storage, sounds, audio_output): Self::SystemData) {
         for (enemy, enemy_transform) in (&mut enemies, &transforms).join() {
             for (blast_entity, blast, blast_transform) in (&*entities, &mut blasts, &transforms).join() {
                 if blast.allied {
@@ -30,6 +38,7 @@ impl<'s> System<'s> for PlayerHitSystem {
 
                     if hitbox_collide(blast_x, blast_y, enemy_x, enemy_y, blast.hitbox_radius, blast.hitbox_radius, enemy.hitbox_width, enemy.hitbox_height) {
                         let _result = entities.delete(blast_entity);
+                        play_sfx(&sounds.spaceship_hit_sfx, &storage, audio_output.as_ref().map(|o| o.deref()));
                         enemy.health -= blast.damage;
                     }
                 }
