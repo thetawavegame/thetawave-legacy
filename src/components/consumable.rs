c21675cab001f6b529c588383917bf47844e007b
Mindaugas Vinkelis <fraillt@gmail.com>
2019-09-05 08:38:34 +0300

    fixed incorrect defaults when loading data, and item spawn now also spawn item in Invasion phase

--- src/components/consumable.rs
+++ src/components/consumable.rs
@@ -3,41 +3,30 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Consumable {
-    #[serde(default)]
+    #[serde(default = "des_width")]
     pub width: f32,
-    #[serde(default)]
+    #[serde(default = "des_height")]
     pub height: f32,
-    #[serde(default)]
+    #[serde(default = "des_hitbox_width")]
     pub hitbox_width: f32,
-    #[serde(default)]
+    #[serde(default = "des_hitbox_height")]
     pub hitbox_height: f32,
+    #[serde(default = "des_speed")]
+    pub speed: f32,
     #[serde(default)]
     pub health_value: f32,
     #[serde(default)]
     pub defense_value: f32,
     #[serde(default)]
     pub money_value: usize,
-    #[serde(default)]
-    pub speed: f32,
-    // this value must be set during deserialization
     pub sprite_index: usize,
 }
 
-impl Default for Consumable {
-    fn default() -> Self {
-        Consumable {
-            width: 12.0,
-            height: 12.0,
-            hitbox_width: 10.0,
-            hitbox_height: 10.0,
-            health_value: 0.0,
-            defense_value: 0.0,
-            money_value: 0,
-            speed: 35.0,
-            sprite_index: 0,
-        }
-    }
-}
+fn des_width() -> f32 { 12.0 }
+fn des_height() -> f32 { 12.0 }
+fn des_hitbox_width() -> f32 { 10.0 }
+fn des_hitbox_height() -> f32 { 10.0 }
+fn des_speed() -> f32 { 35.0 }
 
 impl Component for Consumable {
     type Storage = DenseVecStorage<Self>;
