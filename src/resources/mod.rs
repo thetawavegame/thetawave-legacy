e0026914554f6d063ba3edb972f9689853ac6cf0
Carlo Supina <cdsupina@gmail.com>
2019-09-12 04:04:23 -0400

    move consumables and items to different spritesheets

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,9 +1,11 @@
-pub use self::{
-    sprite::{SpriteResource},
-};
+
 use crate::components::{Enemy, Item, Consumable};
 
 mod sprite;
+
+pub use self::{
+    sprite::{SpriteResource, initialise_sprite_resource},
+};
 
 pub type EnemyPool = std::collections::HashMap<String, Enemy>;
 pub type ItemPool = std::collections::HashMap<String, Item>;
--- src/resources/sprite.rs
+++ src/resources/sprite.rs
@@ -1,10 +1,24 @@
 use amethyst::{
     assets::Handle,
     renderer::SpriteSheet,
+    ecs::prelude::{World}
 };
 
 #[derive(Clone)]
 pub struct SpriteResource {
     pub sprite_sheet: Handle<SpriteSheet>,
+    pub items_sprite_sheet: Handle<SpriteSheet>,
+    pub consumables_sprite_sheet: Handle<SpriteSheet>,
 }
 
+pub fn initialise_sprite_resource(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>, items_sprite_sheet_handle: Handle<SpriteSheet>, consumables_sprite_sheet_handle: Handle<SpriteSheet>) -> SpriteResource {
+    let sprite_resource = SpriteResource {
+        //add different spritesheets here
+        sprite_sheet: sprite_sheet_handle,
+        items_sprite_sheet: items_sprite_sheet_handle,
+        consumables_sprite_sheet: consumables_sprite_sheet_handle,
+    };
+
+    world.add_resource(sprite_resource.clone());
+    sprite_resource
+}
