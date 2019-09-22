86d2ce7d77e26e95f0623f93853f5ba19b51a279
Carlo Supina <cdsupina@gmail.com>
2019-09-22 06:37:20 -0400

    change camera to perspective, added spherical planet

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ extern crate serde;
 
 use amethyst::{
     prelude::*,
+
+    assets::{AssetStorage, Loader, Handle, PrefabLoader, PrefabLoaderSystem, RonFormat, Prefab},
     renderer::{
         RenderingBundle,
         types::DefaultBackend,
-        plugins::{RenderFlat2D, RenderToWindow},
+        plugins::{RenderFlat2D, RenderToWindow, RenderShaded3D, RenderFlat3D},
     },
     utils::application_root_dir,
     core::{transform::TransformBundle},
@@ -57,7 +59,9 @@ fn main() -> amethyst::Result<()> {
                         RenderToWindow::from_config_path(display_config_path)
                             .with_clear([0.0, 0.0, 0.0, 1.0]),
                     )
+                    .with_plugin(RenderFlat3D::default())
                     .with_plugin(RenderFlat2D::default())
+                    //.with_plugin(RenderShaded3D::default())
                     .with_plugin(RenderUi::default())
             )?;
 
