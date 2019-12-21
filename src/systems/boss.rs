fcae7beb430706f4c4205ae2dec6fc37a6f7c570
Carlo Supina <cdsupina@gmail.com>
2019-12-21 05:24:21 -0500

    iterate phase on boss defeat

--- src/systems/boss.rs
+++ src/systems/boss.rs
@@ -2,10 +2,10 @@ use amethyst::{
     core::{
         timing::Time,
     },
-    ecs::prelude::{Entities, Join, System, WriteStorage, Read},
+    ecs::prelude::{Entities, Join, System, WriteStorage, Read, Entity},
 };
 use crate::{
-    components::Repeater,
+    components::{Repeater, GameMaster},
 };
 
 pub struct BossSystem;
@@ -15,12 +15,22 @@ impl<'s> System<'s> for BossSystem {
     type SystemData = (
         Entities<'s>,
         WriteStorage<'s, Repeater>,
+        WriteStorage<'s, GameMaster>,
         Read<'s, Time>,
     );
 
-    fn run(&mut self, (entities, mut repeaters, time): Self::SystemData) {
+    fn run(&mut self, (entities, mut repeaters, mut gamemasters, time): Self::SystemData) {
         for (boss_entity, repeater_component) in (&*entities, &mut repeaters).join() {
 
+            if !entities.is_alive(repeater_component.head) && !entities.is_alive(repeater_component.body){
+                let _result = entities.delete(boss_entity);
+
+                for gamemaster in (&mut gamemasters).join() {
+                    gamemaster.phase_idx += 1;
+                }
+
+                println!("repeater defeated");
+            }
 
         }
     }
