0e90c157e4e89d243584bd06ddb3e4af9279fc86
Carlo Supina <cdsupina@gmail.com>
2020-08-31 22:52:10 -0500

    add hitbox component to spaceship, remove duplicate event generation

--- src/systems/collision_detection.rs
+++ src/systems/collision_detection.rs
@@ -1,5 +1,5 @@
 use crate::{
-    components::{Enemy, Spaceship},
+    components::{Enemy, Hitbox2DComponent, Spaceship},
     space_shooter::CollisionEvent,
     systems::hitbox_collide,
 };
@@ -16,6 +16,7 @@ impl<'s> System<'s> for CollisionDetectionSystem {
     type SystemData = (
         ReadStorage<'s, Enemy>,
         ReadStorage<'s, Spaceship>,
+        ReadStorage<'s, Hitbox2DComponent>,
         ReadStorage<'s, Transform>,
         Entities<'s>,
         Write<'s, EventChannel<CollisionEvent>>,
@@ -23,7 +24,7 @@ impl<'s> System<'s> for CollisionDetectionSystem {
 
     fn run(
         &mut self,
-        (enemies, spaceships, transforms, entities, mut enemy_collision_event_channel): Self::SystemData,
+        (enemies, spaceships, hitboxes, transforms, entities, mut enemy_collision_event_channel): Self::SystemData,
     ) {
         for (entity_a, transform_a, enemy_a) in (&entities, &transforms, &enemies).join() {
             //check for enemy collisions
@@ -60,7 +61,8 @@ impl<'s> System<'s> for CollisionDetectionSystem {
             }
 
             //check for spaceship collisions
-            for (entity_b, transform_b, spaceship_b) in (&entities, &transforms, &spaceships).join()
+            for (entity_b, transform_b, spaceship_b, hitbox_b) in
+                (&entities, &transforms, &spaceships, &hitboxes).join()
             {
                 if hitbox_collide(
                     transform_a.translation().x,
@@ -69,12 +71,12 @@ impl<'s> System<'s> for CollisionDetectionSystem {
                     transform_b.translation().y,
                     enemy_a.hitbox_width,
                     enemy_a.hitbox_height,
-                    spaceship_b.hitbox_width,
-                    spaceship_b.hitbox_height,
+                    hitbox_b.width,
+                    hitbox_b.height,
                     enemy_a.hitbox_x_offset,
                     enemy_a.hitbox_y_offset,
-                    spaceship_b.hitbox_x_offset,
-                    spaceship_b.hitbox_y_offset,
+                    hitbox_b.offset_x,
+                    hitbox_b.offset_y,
                 ) {
                     enemy_collision_event_channel.single_write(CollisionEvent::new(
                         entity_a,
