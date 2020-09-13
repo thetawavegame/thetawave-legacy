b848e6f0297aa8345c89d32966efd2701986f36d
Micah Tigley <tigleym@gmail.com>
2020-09-13 14:21:06 -0400

    Have CI run a strict build that fails on warnings and resolve existing warnings in code.

--- src/entities/boss.rs
+++ src/entities/boss.rs
@@ -35,7 +35,7 @@ pub fn spawn_repeater(
         constants::ARENA_MIN_Y + constants::ARENA_HEIGHT + 98.0,
         constants::BOSS_Z_1,
     );
-    let right_arm_position = Vector3::new(
+    let _right_arm_position = Vector3::new(
         constants::ARENA_MIN_X + (constants::ARENA_WIDTH / 2.0) + 30.0,
         constants::ARENA_MIN_Y + constants::ARENA_HEIGHT + 40.0,
         constants::BOSS_Z_2,
@@ -44,7 +44,7 @@ pub fn spawn_repeater(
     let head_entity_data = enemy_pool[&"repeater_head".to_string()].clone();
     let right_shoulder_entity_data = enemy_pool[&"repeater_right_shoulder".to_string()].clone();
     let left_shoulder_entity_data = enemy_pool[&"repeater_left_shoulder".to_string()].clone();
-    let right_arm_entity_data = enemy_pool[&"repeater_right_arm".to_string()].clone();
+    let _right_arm_entity_data = enemy_pool[&"repeater_right_arm".to_string()].clone();
 
     let body = spawn_enemy(
         &entities,
