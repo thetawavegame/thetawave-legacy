75f750f480425b51fd6e74ecc8114bfd2c9c4135
Carlo Supina <cdsupina@gmail.com>
2021-01-15 11:25:46 -0600

    limit speed of enemies to max_speed, despawn upon reaching the bottom

--- src/systems/motion2d.rs
+++ src/systems/motion2d.rs
@@ -4,7 +4,7 @@ use crate::{
 };
 use amethyst::{
     core::{timing::Time, transform::Transform},
-    ecs::prelude::{Join, Read, ReadStorage, System, WriteStorage},
+    ecs::prelude::{Entities, Join, Read, ReadStorage, System, WriteStorage},
 };
 
 pub struct Motion2DSystem;
@@ -29,6 +29,24 @@ impl<'s> System<'s> for Motion2DSystem {
 
             // update angular position
             transform.append_rotation_z_axis(motion_2d.angular_velocity * dt);
+
+            // limit speed x
+            if motion_2d.velocity.x.abs() > motion_2d.max_speed.x {
+                if motion_2d.velocity.x >= 0.0 {
+                    motion_2d.velocity.x = motion_2d.max_speed.x;
+                } else {
+                    motion_2d.velocity.x = -motion_2d.max_speed.x;
+                }
+            }
+
+            // limit speed y
+            if motion_2d.velocity.y.abs() > motion_2d.max_speed.y {
+                if motion_2d.velocity.y >= 0.0 {
+                    motion_2d.velocity.y = motion_2d.max_speed.y;
+                } else {
+                    motion_2d.velocity.y = -motion_2d.max_speed.y;
+                }
+            }
         }
     }
 }
@@ -37,35 +55,52 @@ pub struct EnemyMotion2DSystem;
 
 impl<'s> System<'s> for EnemyMotion2DSystem {
     type SystemData = (
+        Entities<'s>,
         ReadStorage<'s, EnemyComponent>,
         WriteStorage<'s, Motion2DComponent>,
         WriteStorage<'s, Transform>,
+        ReadStorage<'s, Hitbox2DComponent>,
         Read<'s, Time>,
     );
 
-    fn run(&mut self, (enemies, mut motion_2ds, mut transforms, time): Self::SystemData) {
-        for (enemy, motion_2d, transform) in (&enemies, &mut motion_2ds, &mut transforms).join() {
+    fn run(
+        &mut self,
+        (entities, enemies, mut motion_2ds, mut transforms, hitbox_2ds, time): Self::SystemData,
+    ) {
+        for (enemy, motion_2d, hitbox_2d, transform, entity) in (
+            &enemies,
+            &mut motion_2ds,
+            &hitbox_2ds,
+            &mut transforms,
+            &entities,
+        )
+            .join()
+        {
             match enemy.enemy_type {
                 EnemyType::Pawn => {
                     // accelerate in negative y direction
-                    motion_2d.velocity.y -= motion_2d.acceleration.y;
+                    motion_2d.velocity.y += motion_2d.acceleration.y;
                 }
                 EnemyType::Drone => {
                     // accelerate in negative y direction
-                    motion_2d.velocity.y -= motion_2d.acceleration.y;
+                    motion_2d.velocity.y += motion_2d.acceleration.y;
                 }
                 EnemyType::Hauler => {
                     // accelerate in negative y direction
-                    motion_2d.velocity.y -= motion_2d.acceleration.y;
+                    motion_2d.velocity.y += motion_2d.acceleration.y;
                 }
                 EnemyType::Strafer => {
                     // TODO: Add motion logic
+                    motion_2d.velocity.y += motion_2d.acceleration.y;
+                    motion_2d.velocity.x += motion_2d.acceleration.x;
                 }
                 EnemyType::MissileLauncher => {
                     // TODO: Add motion logic
+                    motion_2d.velocity.y += motion_2d.acceleration.y;
                 }
                 EnemyType::Missile => {
                     // TODO: Add motion logic
+                    motion_2d.velocity.y += motion_2d.acceleration.y;
                 }
                 EnemyType::RepeaterBody => {
                     // TODO: Add motion logic
@@ -79,6 +114,13 @@ impl<'s> System<'s> for EnemyMotion2DSystem {
                 EnemyType::RepeaterArm => {
                     // TODO: Add motion logic
                 }
+            }
+
+            // if enemy reaches the bottom despawn
+            if transform.translation().y - hitbox_2d.height / 2.0 < ARENA_MIN_Y {
+                entities
+                    .delete(entity)
+                    .expect("enemy unable to despawn upon reaching bottom of arena");
             }
         }
     }
@@ -95,6 +137,7 @@ impl<'s> System<'s> for ConstrainToArenaSystem {
 
     fn run(&mut self, (hitboxes, mut transforms, mut motions): Self::SystemData) {
         for (hitbox, transform, motion) in (&hitboxes, &mut transforms, &mut motions).join() {
+            /*
             let x_pos = transform.translation().x - (hitbox.width / 2.0);
             let y_pos = transform.translation().y - (hitbox.height / 2.0);
 
@@ -107,6 +150,7 @@ impl<'s> System<'s> for ConstrainToArenaSystem {
                 motion.velocity.y *= -1.0;
                 motion.acceleration.y *= -1.0;
             }
+            */
         }
     }
 }
