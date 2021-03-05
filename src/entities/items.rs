23b3a66f52d239e0b788f593151a2087a349ca79
Carlo Supina <cdsupina@gmail.com>
2021-03-05 12:20:13 -0600

    remove unused entity files

--- src/entities/items.rs
+++
@@ -1,6 +0,0 @@
-use crate::resources::{ItemEntityData, ItemsResource, SpriteSheetsResource};
-use amethyst::{
-    core::{math::Vector3, transform::Transform, Named},
-    ecs::prelude::{Builder, Entities, LazyUpdate, ReadExpect},
-    renderer::{SpriteRender, Transparent},
-};
